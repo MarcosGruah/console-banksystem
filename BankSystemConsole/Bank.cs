98e4a0413daa4b1bb6ae23c57922c0a232d5dffb
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-12-11 17:10:58 -0300

    feat: Bank Regex, GetAllBanks, DeleteBank

--- BankSystemConsole/Bank.cs
+++ BankSystemConsole/Bank.cs
@@ -1,14 +1,16 @@
-﻿namespace BankSystemConsole
+﻿using System.Text.RegularExpressions;
+
+namespace BankSystemConsole
 {
     internal class Bank
     {
         public Bank(string name, string cnpj, string cep, string phoneNumber)
         {
             Id = Guid.NewGuid();
-            Name = name;
-            Cnpj = cnpj;
-            Cep = cep;
-            PhoneNumber = phoneNumber;
+            Name = name.ToUpper();
+            Cnpj = Regex.Replace(cnpj, "[^0-9]", "");
+            Cep = Regex.Replace(cep, "[^0-9]", "");
+            PhoneNumber = Regex.Replace(phoneNumber, "[^0-9]", "");
         }
         public Guid Id { get; set; }
         public string Name { get; }
--- BankSystemConsole/Program.cs
+++ BankSystemConsole/Program.cs
@@ -5,9 +5,11 @@ Console.WriteLine("Bem Vindo ao Sistema Bancário.\n");
 
 List<Bank> bankList = new List<Bank>();
 
-//bankList.Add(new Bank("Banco do Brasil", "00.000.000/0001-91", "70.040-912", "(61) 3493-9002"));
-//bankList.Add(new Bank("Banco de Brasilia", "00.000.208/0001-00", "70.040-250", "(61) 3409-4039"));
-//bankList.Add(new Bank("Nu Bank", "18.236.120/0001-58", "05409-000", "(11) 2039-0650"));
+Console.WriteLine(bankList.Count == 1 ? $"Existe {bankList.Count} banco registrado no sistema.\n" : $"Existem {bankList.Count} bancos registrados no sistema.\n");
+
+bankList.Add(new Bank("Banco do Brasil", "00.000.000/0001-91", "70.040-912", "(61) 3493-9002"));
+bankList.Add(new Bank("Banco de Brasilia", "00.000.208/0001-00", "70.040-250", "(61) 3409-4039"));
+bankList.Add(new Bank("Nu Bank", "18.236.120/0001-58", "05409-000", "(11) 2039-0650"));
 
 static void AddBank(List<Bank> bankList)
 {
@@ -25,6 +27,69 @@ static void AddBank(List<Bank> bankList)
     Console.Clear();
     Console.WriteLine($"{bankName} adicionado com sucesso!\n");
 }
+
+static void GetAllBanks(List<Bank> bankList)
+{
+    Console.Clear();
+    Console.WriteLine("==============================================\n");
+
+    foreach (var bank in bankList)
+    {
+        Console.WriteLine($"ID: {bank.Id}");
+        Console.WriteLine($"Nome: {bank.Name}");
+        Console.WriteLine($"CNPJ: {bank.Cnpj}");
+        Console.WriteLine($"CEP: {bank.Cep}");
+        Console.WriteLine($"Telefone: {bank.PhoneNumber}\n");
+    }
+    Console.WriteLine("\n==============================================");
+}
+
+static void UpdateBank(List<Bank> bankList)
+{
+};
+
+static void DeleteBank(List<Bank> bankList)
+{
+    Console.Clear();
+    Console.WriteLine("Por favor digite a ID do Banco que deseja remover ou digite 2 para listar todos os bancos: ");
+    string input = Console.ReadLine();
+    if (input == "2")
+    {
+        GetAllBanks(bankList);
+    }
+    else
+    {
+        Bank foundBank = bankList.Find(bank => bank.Id.ToString() == input);
+
+        if (foundBank != null)
+        {
+            do
+            {
+                Console.WriteLine($"Você tem certeza que deseja remover {foundBank.Name}? Esse processo é irreversível.");
+                Console.WriteLine("1 - Sim");
+                Console.WriteLine("2 - Não\n");
+                string confirmDeletion = Console.ReadLine();
+
+
+                switch (confirmDeletion)
+                {
+                    case "1":
+                        bankList.Remove(bankList.Where(bank => bank.Id.ToString() == input).First());
+                        Console.WriteLine("DELETED");
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            } while (true);
+
+        }
+        else
+        {
+            Console.WriteLine("BANCO NÃO ENCONTRADO 404");
+        }
+    }
+};
 
 do
 {
@@ -55,8 +120,6 @@ do
     }
     else
     {
-        Console.WriteLine(bankList.Count == 1 ? $"Existe {bankList.Count} banco registrado no sistema.\n" : $"Existem {bankList.Count} bancos registrados no sistema.\n");
-        Console.WriteLine("Você deseja:\n");
         Console.WriteLine("1 - Registrar um novo banco.");
         Console.WriteLine("2 - Ver a lista de bancos registrados.");
         Console.WriteLine("3 - Atualizar os dados de um banco.");
@@ -71,7 +134,7 @@ do
                 break;
 
             case "2":
-                Console.WriteLine("Ver a lista de bancos registrados.");
+                GetAllBanks(bankList);
                 break;
 
             case "3":
@@ -79,7 +142,7 @@ do
                 break;
 
             case "4":
-                Console.WriteLine("Remover um banco do sistema.");
+                DeleteBank(bankList);
                 break;
 
             case "0":
